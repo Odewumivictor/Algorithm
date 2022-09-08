374ca3583675d5dcc5feb340ea3166d577cf2b41
odewumivictor <vickidubi1@gmail.com>
2022-09-08 16:24:29 +0100

    Add project files.

---
+++ Algorithm/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myNumbers = { 7, 2, 6, 15, 54, 10, 23 };
+            Console.WriteLine(myNumbers.Max()); 
+            Console.WriteLine(myNumbers.Min()); 
+            Console.WriteLine(myNumbers.Sum()); 
+        }
+    }
+}
